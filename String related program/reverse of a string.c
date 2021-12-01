c40d68c1d640ce1eb0ad1f47cdc70be1c875d405
code-with-santanu <santanupal003@gmail.com>
2021-12-02 00:01:46 +0530

    Update reverse of a string.c

--- String related program/reverse of a string.c
+++ String related program/reverse of a string.c
@@ -1,18 +1,20 @@
 /*Print any string in reverse way*/
 
 #include <stdio.h>
-#include<string.h>
+#include <string.h>
+
 int main()
 {
     int a, j;
     char str[20];
     printf("\nEnter any word:");
-    scanf("%s",&str);
-    a=strlen(str)-1;
-    for(j=a;j>=0;j--)
-    {printf("%c",str[j]);
+    scanf("%s", &str);
+    a = strlen(str) - 1;
+
+    for (j = a; j >= 0; j--)
+    {
+        printf("%c", str[j]);
     }
 
     return 0;
 }
-
